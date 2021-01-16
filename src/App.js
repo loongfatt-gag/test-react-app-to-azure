3de35b5ba04517649c14de2a1a4c6d2b4f88db6f
Loong Fatt <loong.fatt@graceaog.org>
2021-01-16 23:31:10 +0800

    Modified Home Page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Landing Page</h1>
+        <h2>Try Committing</h2>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
