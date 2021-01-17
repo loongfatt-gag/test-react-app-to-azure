60bf22d456365aadb4fb3bfead6d61da6ba19840
Loong Fatt <loong.fatt@graceaog.org>
2021-01-17 20:03:00 +0800

    Test Router Dom

--- src/App.js
+++ src/App.js
@@ -1,73 +1,22 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-
-const useStyles = makeStyles((theme) => ({
-  appBar: {
-    backgroundColor: "#fff"
-  },
-  hero: {
-    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('https://images.unsplash.com/photo-1610312200781-0ae27477f220?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxhbGx8fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit')`,
-    height: "300px",
-    backgroundPosition: "center",
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover",
-    position: "relative",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    color: "#fff",
-    fontSize: "4rem",
-  },
-  blogsContainer: {
-    paddingTop: theme.spacing(3)
-  },
-  blogTitle: {
-    fontWeight: 800,
-    paddingBottom: theme.spacing(3)
-  }
-}));
+import Navbar from './components/NavBar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Products from './pages/Projucts';
+import Reports from './pages/Reports';
 
 function App() {
-  const classes = useStyles();
-
   return (
-    <div className="App">
-      <AppBar className={classes.appBar} position="static">
-        <Toolbar>
-          <Typography variant="h6" color="primary" >
-            Azure Static Web App
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Box className={classes.hero}>
-        <Box>React on Azure</Box>
-      </Box>
-      <Container maxwidth="lg" className={classes.blogsContainer}>
-        <Typography variant="h4" className={classes.blogTitle} >
-          Start
-        </Typography>
-        <Grid container spacing={3}>
-          <Grid item xs={12} sm={6} md={4}>
-            Step 1 - Setup Git Repository
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            Step 2 - Create Azure Static Web App
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            Step 3 - Link the Git Repository to Azure Static Web App
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            Step 4 - Allow Github Acitons to push to Azure Static Web App
-          </Grid>
-        </Grid>
-      </Container>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/products' component={Reports} />
+          <Route path='/reports' component={Reports} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
