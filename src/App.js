90135146562573c6309f8b0528ad3aec6f90a61c
Loong Fatt <loong.fatt@graceaog.org>
2021-01-17 13:58:55 +0800

    Added Material UI Blog Layout

--- src/App.js
+++ src/App.js
@@ -3,10 +3,33 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
     backgroundColor: "#fff"
+  },
+  hero: {
+    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('https://images.unsplash.com/photo-1610312200781-0ae27477f220?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxhbGx8fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit')`,
+    height: "300px",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    position: "relative",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    color: "#fff",
+    fontSize: "4rem",
+  },
+  blogsContainer: {
+    paddingTop: theme.spacing(3)
+  },
+  blogTitle: {
+    fontWeight: 800,
+    paddingBottom: theme.spacing(3)
   }
 }));
 
@@ -22,6 +45,28 @@ function App() {
           </Typography>
         </Toolbar>
       </AppBar>
+      <Box className={classes.hero}>
+        <Box>React on Azure</Box>
+      </Box>
+      <Container maxwidth="lg" className={classes.blogsContainer}>
+        <Typography variant="h4" className={classes.blogTitle} >
+          Start
+        </Typography>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6} md={4}>
+            Step 1
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            Step 2
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            Step 3
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            Step 4
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }
